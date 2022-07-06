60df8314d7d686cd134dd0ae742503276ab95b0a
rozanus <roza.nussipbek@gmail.com>
2022-07-06 07:05:18 -0400

    Added Pages and locators, Login Util, config page

---
+++ src/test/java/com/vytrack/pages/LandingPage.java
@@ -0,0 +1,4 @@
+package com.vytrack.pages;
+
+public class LandingPage {
+}
---
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -0,0 +1,29 @@
+package com.vytrack.pages;
+
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "prependedInput")
+    public WebElement username;
+
+    @FindBy(id = "prependedInput2")
+    public WebElement password;
+
+    @FindBy(className = "custom-checkbox__text")
+    public WebElement rememberMeCheckBox;
+
+    @FindBy(id = "_submit")
+    public WebElement loginBtn;
+
+
+
+
+}
