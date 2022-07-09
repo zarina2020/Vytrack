ae5d8eb25c38284a69d08faf59722f14eafa23d9
anast <anastasiyagok81@hotmail.com>
2022-07-08 19:17:16 -0700

    Anastasiya updated 1

--- src/test/java/com/vytrack/stepDefs/RefreshButtonStepDefs.java
+++ src/test/java/com/vytrack/stepDefs/RefreshButtonStepDefs.java
@@ -10,17 +10,17 @@ import io.cucumber.java.en.Then;
 
 public class RefreshButtonStepDefs {
 
-    LoginPage loginPage = new LoginPage();
-    VehiclesPage vehiclesPage = new VehiclesPage();
-
-    @Given("User on the Vehicles page")
-    public void user_on_the_vehicles_page() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("vytrack"));
-        String username = ConfigurationReader.getProperty("username");
-        String password = ConfigurationReader.getProperty("password");
-
-        loginPage.login(username,password);
-    }
+//    LoginPage loginPage = new LoginPage();
+//    VehiclesPage vehiclesPage = new VehiclesPage();
+//
+//    @Given("User on the Vehicles page")
+//    public void user_on_the_vehicles_page() {
+//        Driver.getDriver().get(ConfigurationReader.getProperty("vytrack"));
+//        String username = ConfigurationReader.getProperty("username");
+//        String password = ConfigurationReader.getProperty("password");
+//
+//        loginPage.login(username,password);
+//    }
 //    @Then("User should be able to click refresh button")
 //    public void user_should_be_able_to_click_refresh_button() {
 //       vehiclesPage.refreshIcon.click();
