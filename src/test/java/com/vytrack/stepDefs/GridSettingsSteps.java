b28755efbbacd52dc9a7b1b27d1340ab1791d2ea
kathe <kathevpp@gmail.com>
2022-07-10 22:25:07 -0400

    Kathe test

--- src/test/java/com/vytrack/stepDefs/GridSettingsSteps.java
+++ src/test/java/com/vytrack/stepDefs/GridSettingsSteps.java
@@ -1,17 +1,22 @@
 package com.vytrack.stepDefs;
 
+import com.vytrack.pages.VehiclesPage;
+import com.vytrack.utilities.VehiclesPageLogin;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
 public class GridSettingsSteps {
     @Given("user is on Vehicle page")
     public void user_is_on_vehicle_page() {
-        System.out.println("User is on the Vehicle page");
+        VehiclesPageLogin.vehiclesPageLogin();
+
 
     }
     @Then("user should be able to click Grid Settings button")
-    public void user_should_be_able_to_click_grid_settings_button() {
-        System.out.println("User should be able to click Grid Setting Button");
+    public void user_should_be_able_to_click_grid_settings_button(String string) {
+        VehiclesPage vehiclesPage = new VehiclesPage();
+        VehiclesPage.gridSettingsIcon.click();
+
 
 
     }
